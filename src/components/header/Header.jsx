cffe656515f8406eeed57bed5c4b8c9f387cae94
VinicinBraga <vinicius@bragamatta.com.br>
2023-01-10 16:25:46 -0300

    Header and CTA

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import CTA from "./CTA";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Vinícius Braga Matta</h1>
+        <h5 className="text-light">Front-end Developer</h5>
+        <CTA />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
