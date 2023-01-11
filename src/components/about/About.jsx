22aa40a119da67103f7fd8a8955345fe8cbfe759
VinicinBraga <vinicius@bragamatta.com.br>
2023-01-11 17:43:58 -0300

    About

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,52 @@
 import React from "react";
+import "./about.css";
+import ME from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUser } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me-image">
+            <img src={ME} alt="Me"></img>
+          </div>
+        </div>
+        <div className="about_content">
+          <div className="about_cards">
+            <article className="about_card">
+              <FaAward className="about_icon" />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+            <article className="about_card">
+              <FiUser className="about_icon" />
+              <h5>Clients</h5>
+              <small>200+ Wordwide</small>
+            </article>
+            <article className="about_card">
+              <VscFolderLibrary className="about_icon" />
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Perspiciatis mollitia quos saepe debitis, voluptatibus voluptate
+            animi consequatur odit reiciendis placeat aliquam alias cumque fuga
+            distinctio quidem sequi, inventore asperiores! Nisi?
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
