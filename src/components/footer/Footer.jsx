b6248138b1d42a8f2465e35ba7291de616f09d96
VinicinBraga <vinicius@bragamatta.com.br>
2023-01-16 16:02:59 -0300

    Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,55 @@
 import React from "react";
+import "./footer.css";
+import { FaFacebookF } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
+import { IoLogoTwitter } from "react-icons/io";
 
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <footer>
+      <a href="#" className="footer_logo">
+        Vinícius Braga
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portifolio">Portifolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footer_socials">
+        <a href="https://facebook.com">
+          <FaFacebookF />
+        </a>
+        <a href="https://instagram.com">
+          <FiInstagram />
+        </a>
+        <a href="https://twitter.com">
+          <IoLogoTwitter />
+        </a>
+      </div>
+      <div className="footer_copyright">
+        <small>&copy; Vinícius Braga Matta</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
