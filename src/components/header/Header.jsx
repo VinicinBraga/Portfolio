ef385dc22a61522b064d856d9c97578d21ee7244
VinicinBraga <vinicius@bragamatta.com.br>
2023-01-31 16:18:02 -0300

    CSS Update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     <header>
       <div className="container header_container">
         <h4>Hello I'm</h4>
-        <h1>Vinícius Braga Matta</h1>
+        <h2>Vinícius Braga Matta</h2>
         <h4 className="text-light">Front-end Developer</h4>
         <CTA />
         <HeaderSocial />
